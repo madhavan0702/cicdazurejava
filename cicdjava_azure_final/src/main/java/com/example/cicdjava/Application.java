33428a8c0b18344963da5f9a11321072f2ab6f68
madhavan0702 <madhavanm994@gmail.com>
2025-09-16 07:17:46 -0700

    Add files via upload

---
+++ cicdjava_azure_final/src/main/java/com/example/cicdjava/Application.java
@@ -0,0 +1,21 @@
+package com.example.cicdjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello from cicdjava on Azure!";
+    }
+}
